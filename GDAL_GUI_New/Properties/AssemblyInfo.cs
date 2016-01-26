0e4c108b1c5bf900de6b9691ba7019fc09371683
Фоминых Алексей Андреевич <foma1994@yandex.ru>
2016-01-26 22:58:33 +0300

    Изменил копирайт

--- GDAL_GUI_New/Properties/AssemblyInfo.cs
+++ GDAL_GUI_New/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GDAL_GUI_New")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © Фоминых Алексей 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -53,3 +53,5 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
+
