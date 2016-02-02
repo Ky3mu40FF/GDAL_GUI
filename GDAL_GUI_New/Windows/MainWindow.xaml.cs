bc14ee77f7ef3cfa90c913fe41330336432a511d
Фоминых Алексей Андреевич <foma1994@yandex.ru>
2016-02-03 00:05:39 +0300

    Добавил новые пункты меню (Добавить задачу, Запуск всех задач, Запуск выделенной задачи); Добавил элементы для вывода текстовых результатов работы утилит и для вывода всех добавленных задач; Окно теперь появляется в центре экрана

--- GDAL_GUI_New/Windows/MainWindow.xaml.cs
+++ GDAL_GUI_New/Windows/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
 using Gat.Controls;
 
 namespace GDAL_GUI_New
@@ -22,46 +23,80 @@ namespace GDAL_GUI_New
     public partial class MainWindow : Window
     {
         // Переменные
+        #region Переменные
 
-        
-        
+        #endregion
+
+        // Свойства
+        #region Свойства
+
+        #endregion
+
         // Конструкторы
+        #region Конструкторы
 
         public MainWindow()
         {
             InitializeComponent();
-
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
             EventAndPropertiesInitialization();
         }
+        #endregion
 
         // Обработчики событий
-
+        #region Обработчики событий
         // Выход из приложения по нажатию на кнопку Выход в меню
         private void Menu_File_Exit_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown(0);
         }
-
+        // Добавление нового задания
+        private void Menu_Edit_AddTask_Click(object sender, RoutedEventArgs e)
+        {
+            TaskEditWindow taskEditWindow = new TaskEditWindow();
+            taskEditWindow.ShowDialog();
+        }
+        // Запуск всех добавленных заданий по порядку
+        private void Menu_Run_RunAll_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Заглушка. Run All");
+        }
+        // Запуск одного выбранного задания
+        private void Menu_Run_RunSelected_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Заглушка. Run Selected");
+        }
+        // Открытие окна настроек
         private void Menu_Settings_Click(object sender, RoutedEventArgs e)
         {
             SettingsWindow settingsWindow = new SettingsWindow();
             settingsWindow.ShowDialog();
         }
 
+        // Открытие окна "О программе"
         private void Menu_About_Click(object sender, RoutedEventArgs e)
         {
             About about = new About();
             about.Show();
         }
 
-        // Остальные функции
+        #endregion
 
+        // Остальные функции
+        #region Остальные функции
         private void EventAndPropertiesInitialization()
         {
             // Подписка на события
             Menu_File_Exit.Click += new RoutedEventHandler(Menu_File_Exit_Click);
+            Menu_Edit_AddTask.Click += new RoutedEventHandler(Menu_Edit_AddTask_Click);
+            Menu_Run_RunAll.Click += new RoutedEventHandler(Menu_Run_RunAll_Click);
+            Menu_Run_RunSelected.Click += new RoutedEventHandler(Menu_Run_RunSelected_Click);
             Menu_Settings.Click += new RoutedEventHandler(Menu_Settings_Click);
             Menu_About.Click += new RoutedEventHandler(Menu_About_Click);
         }
+
+        #endregion
     }
+
+    
 }
