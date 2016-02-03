071e1562485a556e2cd7c7ff9d5328c2f5eab88d
Фоминых Алексей Андреевич <foma1994@yandex.ru>
2016-02-03 23:21:11 +0300

    Создал класс-обёртку, чтобы мог переопределить метод ToString, который будет использоваться в ListBox при выводе

---
+++ GDAL_GUI_New/Classes/MyDataRow.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace GDAL_GUI_New
+{
+    public sealed class MyDataRow
+    {
+        private DataRow m_Row;
+        public MyDataRow(DataRow row)
+        {
+            m_Row = row;
+        }
+
+        public DataRow GetDataRow
+        {
+            get { return m_Row; }
+        }
+
+        public override string ToString()
+        {
+            return this.m_Row["NameOfParam"].ToString();
+        }
+    }
+}
