22388fd42204c3a070ef745afe5fe2c0ca1acd25
Фоминых Алексей Андреевич <foma1994@yandex.ru>
2016-02-03 23:24:04 +0300

    Добавил класс-обёртку на StringBuilder, который нужен, чтобы получать и выводить выходную информацию от утилит

---
+++ GDAL_GUI_New/Classes/MyStringBuilder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace GDAL_GUI_New
+{
+    class MyStringBuilder : INotifyPropertyChanged
+    {
+        private StringBuilder m_StringBuilder;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //private void OnPropertyChanged(string propertyName)
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public MyStringBuilder()
+        {
+            m_StringBuilder = new StringBuilder();
+        }
+
+        public String Text
+        {
+            get { return this.m_StringBuilder.ToString(); }
+            set
+            {
+                this.m_StringBuilder.Append(value);
+                OnPropertyChanged("Text");
+            }
+        }
+    }
+}
