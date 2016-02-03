a122c19b0187c2311255860cc042d37eee7674f2
Фоминых Алексей Андреевич <foma1994@yandex.ru>
2016-02-03 23:22:38 +0300

    Добавил файл с классами-исключениями. Пока добавил только исключение NotEditingException, которое вызывается, если попытаться отредактировать задачу не в режиме редактирования

---
+++ GDAL_GUI_New/Classes/MyExceptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace GDAL_GUI_New
+{
+    class NotEditingException : Exception
+    {
+        public NotEditingException() :
+            base("Не включен режим редактирования процесса.")
+        { }
+    }
+}
