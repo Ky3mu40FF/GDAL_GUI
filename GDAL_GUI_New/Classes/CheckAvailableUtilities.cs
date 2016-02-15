bf5b3df1b247cf8f97c9a47dc6a4ffb96ae0b065
Фоминых Алексей Андреевич <foma1994@yandex.ru>
2016-02-15 23:49:02 +0300

    Добавил статический класс для проверки наличия поддерживаемых программой утилит в указанной папке

---
+++ GDAL_GUI_New/Classes/CheckAvailableUtilities.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GDAL_GUI_New
+{
+    public static class CheckAvailableUtilities
+    {
+        public static int GetNumOfAvailableUtilities(string path, List<string> utilitiesNames)
+        {
+            int numOfAvailableUtilities = 0;
+            try
+            {
+                List<string> utilitiesInDirectory = Directory.GetFiles(path, "gdal*.exe").ToList<string>();
+                if (utilitiesInDirectory.Count > 0)
+                {
+                    for (int i = 0; i < utilitiesInDirectory.Count; i++)
+                    {
+                        utilitiesInDirectory[i] = 
+                            Path.GetFileNameWithoutExtension(utilitiesInDirectory[i]);
+                    }
+                }
+                List<string> availableUtilities = 
+                    utilitiesNames.Intersect(utilitiesInDirectory).ToList<string>();
+                numOfAvailableUtilities = availableUtilities.Count;
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("Не удалось определить количество доступных утилит" + 
+                    Environment.NewLine + e.Message);
+            }
+            return numOfAvailableUtilities;
+        }
+
+        public static List<string> GetListOfAvailableUtilities(string path, List<string> utilitiesNames)
+        {
+            List<string> availableUtilities = new List<string>();
+            try
+            {
+                List<string> utilitiesInDirectory = Directory.GetFiles(path, "gdal*.exe").ToList<string>();
+                if (utilitiesInDirectory.Count > 0)
+                {
+                    for (int i = 0; i < utilitiesInDirectory.Count; i++)
+                    {
+                        utilitiesInDirectory[i] =
+                            Path.GetFileNameWithoutExtension(utilitiesInDirectory[i]);
+                    }
+                }
+                availableUtilities =
+                    utilitiesNames.Intersect(utilitiesInDirectory).ToList<string>();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Не удалось определить количество доступных утилит" +
+                    Environment.NewLine + e.Message);
+            }
+            return availableUtilities;
+        }
+    }
+}
