f1e598521ab58186c13c81fee60e69f1b7470779
Фоминых Алексей Андреевич <foma1994@yandex.ru>
2016-02-15 23:50:25 +0300

    Добавил выбор папки с утилитами (а также проверки на наличие совместимых утилит).

--- GDAL_GUI_New/Windows/SettingsWindow.xaml.cs
+++ GDAL_GUI_New/Windows/SettingsWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Forms;
 using System.Globalization;
 
 namespace GDAL_GUI_New
@@ -20,9 +21,18 @@ namespace GDAL_GUI_New
     /// </summary>
     public partial class SettingsWindow : Window
     {
+        // Переменные
+            #region Переменные
+        private string m_BundledUtilitiesPath = @"Resources\GDAL_Bundle\";
+        #endregion
+
+        // Конструкторы
+        #region Конструкторы
         public SettingsWindow()
         {
             InitializeComponent();
+
+            EventsAndOtherProperties();
 
             App.LanguageChanged += LanguageChanged;
             CultureInfo currentLanguage = App.Language;
@@ -32,11 +42,35 @@ namespace GDAL_GUI_New
             {
                 comboBox_ProgramLanguage.Items.Add(lang);
             }
+            comboBox_ProgramLanguage.SelectedItem = Properties.Settings.Default.ProgramLanguage;
 
-            comboBox_ProgramLanguage.SelectionChanged += 
-                new SelectionChangedEventHandler(ComboBox_ProgramLanguage_SelectionChanged);
+            comboBox_ContentLanguage.Items.Clear();
+            comboBox_ContentLanguage.ItemsSource = new string[] {"Eng", "Rus"};
+            comboBox_ContentLanguage.SelectedItem = Properties.Settings.Default.DescriptionsLanguage;
+
+            checkBox_UseBundledUtilities.IsChecked = Properties.Settings.Default.UseTheBundledUtilities;
         }
+        #endregion
 
+        // Свойства
+            #region Свойства
+
+        #endregion
+
+        // Методы
+            #region Методы
+
+        private void EventsAndOtherProperties()
+        {
+            button_SaveSettings.Click +=
+                new RoutedEventHandler(Button_SaveSettings_Click);
+            button_BrowseUtilitiesFolderPath.Click +=
+                new RoutedEventHandler(Button_BrowseUtilitiesFolderPath_Click);
+            checkBox_UseBundledUtilities.Checked +=
+                new RoutedEventHandler(CheckBox_UseBundledUtilities_Checked);
+            checkBox_UseBundledUtilities.Unchecked +=
+                new RoutedEventHandler(CheckBox_UseBundledUtilities_Unchecked);
+        }
 
         private void LanguageChanged(object sender, EventArgs e)
         {
@@ -44,20 +78,140 @@ namespace GDAL_GUI_New
             comboBox_ProgramLanguage.SelectedItem = currLang;
         }
 
-        private void ComboBox_ProgramLanguage_SelectionChanged(object sender, SelectionChangedEventArgs e)
+
+        #endregion
+
+        // Обработчики событий
+            #region Обработчики событий
+
+        private void Button_SaveSettings_Click(object sender, RoutedEventArgs e)
         {
-            ComboBox cB = sender as ComboBox;
-            if (cB.SelectedItem != null)
+            // Сохранение состояния CheckBox элемента
+            if (checkBox_UseBundledUtilities.IsChecked != null)
             {
-                CultureInfo lang = cB.SelectedItem as CultureInfo;
+                Properties.Settings.Default.UseTheBundledUtilities =
+                    (bool) checkBox_UseBundledUtilities.IsChecked;
+            }
+
+            // Сохранение выбранного языка программы
+            if (comboBox_ProgramLanguage.SelectedItem != null)
+            {
+                CultureInfo lang = comboBox_ProgramLanguage.SelectedItem as CultureInfo;
                 if (lang != null)
                 {
                     App.Language = lang;
                 }
             }
 
+            // Сохранение выбранного языка описаний
+            if (comboBox_ContentLanguage.SelectedItem != null)
+            {
+                Properties.Settings.Default.DescriptionsLanguage =
+                    comboBox_ContentLanguage.SelectedItem.ToString();
+            }
+
+            // Сохранение всех изменений в настройках
+            Properties.Settings.Default.Save();
+            System.Windows.MessageBox.Show("Изменения сохранены!", "Успех!",
+                MessageBoxButton.OK, MessageBoxImage.Information);
         }
 
+        private void Button_BrowseUtilitiesFolderPath_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            if (folderBrowserDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                DataBaseControl.ConnectToDB();
+                try
+                {
+                    int numOfAvailableUtilities = 0;
+
+                    numOfAvailableUtilities = CheckAvailableUtilities.GetNumOfAvailableUtilities(
+                        folderBrowserDialog.SelectedPath,
+                        DataBaseControl.GetUtilitiesNames());
+                    if (numOfAvailableUtilities > 0)
+                    {
+                        System.Windows.MessageBox.Show("Найдено поддерживаемых утилит: " + numOfAvailableUtilities,
+                            "Успех!", MessageBoxButton.OK, MessageBoxImage.Information);
+                        Properties.Settings.Default.UtilitiesDirectory =
+                            folderBrowserDialog.SelectedPath + "\\";
+                    }
+                    else
+                    {
+                        System.Windows.MessageBox.Show("В указанной папке не найдены поддерживаемые утилиты.\n" +
+                                                       "Программа переключится на встроенный пакет утилит.",
+                            "Внимание!", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        checkBox_UseBundledUtilities.IsChecked = true;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    System.Windows.MessageBox.Show(
+                        "Произошла ошибка. Не удалось установить папку с утилитами." +
+                        Environment.NewLine + ex.Data,
+                        "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                finally
+                {
+                    DataBaseControl.CloseConnection();
+                }
+            }
+        }
+
+        private void CheckBox_UseBundledUtilities_Checked(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Controls.CheckBox cB = sender as System.Windows.Controls.CheckBox;
+
+            DataBaseControl.ConnectToDB();
+            try
+            {
+                int numOfAvailableUtilities = 0;
+
+                numOfAvailableUtilities = CheckAvailableUtilities.GetNumOfAvailableUtilities(
+                    m_BundledUtilitiesPath,
+                    DataBaseControl.GetUtilitiesNames());
+                if (numOfAvailableUtilities > 0)
+                {
+                    System.Windows.MessageBox.Show("Найдено поддерживаемых утилит: " + numOfAvailableUtilities,
+                        "Успех!", MessageBoxButton.OK, MessageBoxImage.Information);
+                    Properties.Settings.Default.UtilitiesDirectory = m_BundledUtilitiesPath;
+                    textBox_UtilitiesFolderPath.IsEnabled = false;
+                    button_BrowseUtilitiesFolderPath.IsEnabled = false;
+                }
+                else
+                {
+                    TextBlock tBlock = checkBox_UseBundledUtilities.Content as TextBlock;
+                    System.Windows.MessageBox.Show(
+                        "Не найден комплектный пакет утилит." + Environment.NewLine +
+                        "Утилиты должны распологаться по пути:" + Environment.NewLine + 
+                        m_BundledUtilitiesPath + Environment.NewLine +
+                        "Пожалуйста, выберите папку, в которой есть поддерживаемые утилиты, " +
+                        "или же добавьте их в каталог \"" + m_BundledUtilitiesPath + 
+                        "\" и отметьте поле \"" + tBlock.Text + "\".",
+                        "Внимание!", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    cB.IsChecked = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show(
+                    "Произошла ошибка. Не удалось установить папку с утилитами." +
+                    Environment.NewLine + ex.Data,
+                    "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            finally
+            {
+                DataBaseControl.CloseConnection();
+            }
+        }
+
+        private void CheckBox_UseBundledUtilities_Unchecked(object sender, RoutedEventArgs e)
+        {
+            textBox_UtilitiesFolderPath.IsEnabled = true;
+            button_BrowseUtilitiesFolderPath.IsEnabled = true;
+        }
+
+        #endregion
 
     }
 }
