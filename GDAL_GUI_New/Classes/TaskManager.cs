f6a02e432c827cadbfea4d41376016e09b083988
Фоминых Алексей Андреевич <foma1994@yandex.ru>
2016-02-20 22:56:07 +0300

    Начал писать менеджер заданий

---
+++ GDAL_GUI_New/Classes/TaskManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.ObjectModel;
+
+namespace GDAL_GUI_New
+{
+    public static class TaskManager
+    {
+        // Переменные
+            #region Переменные
+        private static ObservableCollection<MyTask> m_Tasks;
+        #endregion
+
+        // Конструкторы
+        #region Конструкторы
+
+        #endregion
+
+        // Свойства
+        #region Свойства
+
+        public static ObservableCollection<MyTask> TasksCollection
+        {
+            get { return m_Tasks; }
+            set
+            {
+                if (value != null)
+                {
+                    m_Tasks = value;
+                }
+            }
+        }
+        #endregion
+
+        // Методы
+        #region Методы
+
+        public static void InitializeProcessManager()
+        {
+            
+        }
+        #endregion
+
+        // Обработчики событий
+        #region Обработчики событий
+
+        #endregion
+    }
+}
